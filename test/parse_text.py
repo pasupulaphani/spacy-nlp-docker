c05a36c6df5b0580b02f06297dbe2a94461eaef8
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-12-21 22:27:59 +0000

    added test file

---
+++ test/parse_text.py
@@ -0,0 +1,29 @@
+import spacy
+
+nlp = spacy.load('en')
+
+def noun_chunks(text):
+
+    result = []
+    doc = nlp(text)
+
+    for np in doc.noun_chunks:
+        print(np.text)
+        result.append(np.text)
+
+    return result
+
+
+input = u'hotel new york'
+expected = ['hotel', 'new york']
+
+actual = noun_chunks(input)
+print('expected: ')
+print(expected)
+print('actual: ')
+print(actual)
+
+if cmp(expected, actual) == 0:
+    print('Noun chunks successful')
+else:
+    raise Error('Noun chunks not same')

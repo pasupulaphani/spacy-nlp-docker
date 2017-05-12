c370b17f19983c7923db0f8e694fa97096a888b7
phaninder pasupula <pasupulaphani@gmail.com>
2017-05-12 15:03:03 +0100

    Temp fix to support Pearlshare client

--- zeromq/server.py
+++ zeromq/server.py
@@ -20,21 +20,21 @@ logging.info("Loaded model: 'en'")
 
 class SpacyNlpRPC(object):
     def parse(self, bytes):
-        text = bytes.decode('utf-8')
+        text = bytes
         print('parse: text - ' + text)
         p = Parse(enNlpModel, text)
         res = json.dumps(p.to_json(), sort_keys=True)
         logging.debug('parse: result - ' + res)
         return res
     def entities(self, bytes):
-        text = bytes.decode('utf-8')
+        text = bytes
         logging.debug('entities: text - ' + text)
         e = Entities(enNlpModel, text)
         res = json.dumps(e.to_json(), sort_keys=True)
         logging.debug('entities: result - ' + res)
         return res
     def nounChunks(self, bytes):
-        text = bytes.decode('utf-8')
+        text = bytes
         logging.debug('nounChunks: text - ' + text)
         ncs = NounChunks(enNlpModel, text)
         res = json.dumps(ncs.to_json(), sort_keys=True)
